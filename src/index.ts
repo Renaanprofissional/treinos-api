9dde98e719415b88049f22095e3d6f461d84b4e6
Renaanprofissional <renaan.profissional@gmail.com>
2026-03-24 20:47:09 -0300

    feat: add prisma db

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,66 @@
-console.log("Hello Word");
-export {};
+import "dotenv/config";
+
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+import Fastify from "fastify";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import z from "zod";
+
+const app = Fastify({
+  logger: true,
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Bootcamp Treinos API",
+      description: "API para o bootcamp de treinos do FSC",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        description: "Localhost",
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
+});
+
+await app.register(fastifySwaggerUI, {
+  routePrefix: "/docs",
+});
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/",
+  schema: {
+    description: "Hello word",
+    tags: ["Hello word"],
+    response: {
+      200: z.object({
+        message: z.string(),
+      }),
+    },
+  },
+  handler: () => {
+    return {
+      message: "hello word",
+    };
+  },
+});
+
+try {
+  await app.listen({ port: Number(process.env.PORT) || 3000 });
+} catch (err) {
+  app.log.error(err);
+  process.exit(1);
+}
